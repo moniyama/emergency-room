a3446963756566af1560cafbbf454801a737ce21
Monica <monicasayama@gmail.com>
2022-10-18 19:12:39 -0300

    ✨feature: add login firebase login

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,20 +4,22 @@ import { redirect } from "../utils.js"
 export default function () {
   const container = document.createElement("section")
   container.innerHTML = `
-    <input type="email" placeholder="email">
-    <input type="password" placeholder="password">
+    <input id="email" type="email" placeholder="email">
+    <input id="password" type="password" placeholder="password">
     <button id="btn-login">LOGIN</button>
     <button id="btn-sign-up">Cadastrar</button>
   `
 
   container.querySelector("#btn-login").addEventListener("click", () => {
-    loginWithFirebase()
+    const email = container.querySelector("#email").value
+    const password = container.querySelector("#password").value
+    loginWithFirebase(email, password)
       .then(() => redirect("#tele"))
   })
 
   container.querySelector("#btn-sign-up").addEventListener("click", () => {
-    createAccountWithFirebase()
-      .then(() => redirect("#tele"))
+    redirect("#cadastro")
   })
+
   return container
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,10 @@
-export function loginWithFirebase() {
-  return Promise.resolve()
+import { auth } from "./firebase-config.js"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+
+export function loginWithFirebase(email, password) {
+  return signInWithEmailAndPassword(auth, email, password)
 }
 
-export function createAccountWithFirebase() {
-  return Promise.resolve()
+export function createAccountWithFirebase(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password)
 }
