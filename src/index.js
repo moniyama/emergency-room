984ac661da48693dcfd833617fc69875802aceec
Monica <monicasayama@gmail.com>
2022-10-21 19:03:05 -0300

    🖥️ wip: add header

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import signup from "./pages/signup.js"
 import rooms from "./pages/rooms.js"
 import patientForm from "./pages/patient-form.js"
 import { isLoggedUser } from "./services/firebase.js"
+import header from "./components/header.js"
 function routes() {
   const root = document.querySelector("#root")
   root.innerHTML = ""
@@ -15,9 +16,11 @@ function routes() {
       root.appendChild(signup())
       break
     case "#tele":
+      root.appendChild(header())
       root.appendChild(rooms())
       break
     case "#patient-form":
+      root.appendChild(header())
       root.appendChild(patientForm())
       break
     default:
