6b4581acd3ef24315c66dea3a669cdb77c594c12
Monica <monicasayama@gmail.com>
2022-10-21 18:45:41 -0300

    🖥️ wip: calling next patient in room

--- src/pages/rooms.js
+++ src/pages/rooms.js
@@ -10,10 +10,26 @@ export default function () {
       <h1>Lista de espera</h1>
       <ul id="all-patients"></ul>
     </aside>
-    <section>rooms</section>
+    <section>
+      <h1>rooms</h1>
+      <ul id="all-rooms">
+        <li class="room">
+          <h2>Room 1</h2>
+          <div></div>
+          <button class="next-patient">proximo</button>
+        </li>
+        <li class="room">
+          <h2>Room 2</h2>
+          <div></div>
+          <button class="next-patient">proximo</button>
+        </li>
+      </ul>
+    </section>
     <button id="patients-form">Cadastro de paciente</button>
-  `
+    `
   const patients = []
+  const rooms = [] // [{ number: 1, user: {} }, { number: 2, user: {} }]
+
   getPatients()
     .then(querySnapshot => {
       querySnapshot.forEach((doc) => {
@@ -25,13 +41,36 @@ export default function () {
   function printPatients(list) {
     container.querySelector("#all-patients").innerHTML = list.map(patient => {
       return `
-        <li id=${patient.id}>
+        <li class="patient" id=${patient.id}>
           <div>${patient.name}</div>
           <div>${patient.severity}</div>
         </li>
       `
     }).join("")
   }
+
+  function printRooms(list) {
+    container.querySelector("#all-rooms").innerHTML = list.map(room => {
+      return `
+        <li class="room">
+          <h2>Room ${room.number}</h2>
+          <div>${room.user.name}</div>
+        </li>    
+      `
+    }).join("")
+  }
+
+  container.querySelectorAll(".next-patient").forEach(el => el.addEventListener("click", (e) => {
+    const nextPatient = container.querySelector(".patient")
+    if (nextPatient) {
+      e.currentTarget.previousElementSibling.prepend(nextPatient)
+    }
+    const thisRoomPatient = e.currentTarget.previousElementSibling.children
+    const clickedRoomHasAlreadyOnePatient = thisRoomPatient.length > 1
+    if (clickedRoomHasAlreadyOnePatient) {
+      thisRoomPatient[1].remove()
+    }
+  }))
 
   container.querySelector("#patients-form").addEventListener("click", () => {
     redirect("#patient-form")
